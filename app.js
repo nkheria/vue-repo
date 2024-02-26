94e1c924dca0ee9adc16d9fa90f2bbaa8daa5a9f
Neeraj <nkheria@gmail.com>
2024-02-26 11:57:33 +0530

    code with assignment 1 and 2 folders

--- app.js
+++ app.js
@@ -1,31 +1,22 @@
-// const buttonEl = document.querySelector('button');
-// const inputEl = document.querySelector('input');
-// const listEl = document.querySelector('ul');
-
-// function addGoal() {
-
-//     const enteredValue = inputEl.value;
-//     const listItemEl = document.createElement('li');
-//     listItemEl.textContent = enteredValue;
-//     listEl.appendChild(listItemEl);
-//     inputEl.value = '';
-// }
-
-// buttonEl.addEventListener('click', addGoal);
-
-Vue.createApp({
+const app = Vue.createApp({
     data() {
         return {
-            goals: [],
-            enteredValue: ''
+            goalA: 'Finish the course and learn Vue!',
+            goalB: 'Master VueJS and build apps!',
+            vueLink: 'https://vuejs.org/'
         };
-        },
+    },
 
     methods: {
-        addGoal() {
-            this.goals.push(this.enteredValue);
-            this.enteredValue = '';
+        outputGoal() {
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5) {
+                return this.goalA;
+            } else {
+                return this.goalB;
         }
     }
+    }
+});
 
-}).mount('#app');
+app.mount('#user-goal')
