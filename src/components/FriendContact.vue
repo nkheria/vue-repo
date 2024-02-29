7b042a4aaf4d10e5094ee364a38e712def71687f
Neeraj <nkheria@gmail.com>
2024-02-29 12:11:12 +0530

    props-01

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,38 @@
+<template>
+    <li>
+        <h2>{{ name }}</h2>
+        <button @click="toggleDetails"> Show Details </button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone:</strong> {{ phoneNumber }}</li>
+            <li><strong>Email:</strong> {{ emailId }}</li>
+        </ul>
+        </li>
+</template>
+
+<script>
+export default {
+
+    props:[
+        'name',
+        'phoneNumber',
+        'emailId'
+    ],
+    data() {
+        return {
+            detailsAreVisible: false,
+            friend : {
+                id: "manuel",
+                name: "Manuel Lorenz",
+                phone: "01234 5678 991",
+                email: "mauel@example.com"
+        },
+    };
+},
+    methods: {
+        toggleDetails() {
+        this.detailsAreVisible = !this.detailsAreVisible;
+        this.phoneNumber
+    }
+}
+};
+</script>
