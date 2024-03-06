6527ce68efa9c8bfeef291bfbb9f9c543aaeb429
Neeraj <nkheria@gmail.com>
2024-03-06 12:07:09 +0530

    firebase01

--- src/components/survey/LearningSurvey.vue
+++ src/components/survey/LearningSurvey.vue
@@ -47,7 +47,7 @@ export default {
       invalidInput: false,
     };
   },
-  emits: ['survey-submit'],
+  // emits: ['survey-submit'],
   methods: {
     submitSurvey() {
       if (this.enteredName === '' || !this.chosenRating) {
@@ -56,11 +56,21 @@ export default {
       }
       this.invalidInput = false;
 
-      this.$emit('survey-submit', {
-        userName: this.enteredName,
-        rating: this.chosenRating,
-      });
+      // this.$emit('survey-submit', {
+      //   userName: this.enteredName,
+      //   rating: this.chosenRating,
+      // });
 
+    fetch('https://new-vue-app-bfe08-default-rtdb.firebaseio.com/surveys.json',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: this.enteredName,
+        rating: this.chosenRating,
+      }),
+    });
       this.enteredName = '';
       this.chosenRating = null;
     },
